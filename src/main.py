2163c6e344e60c79cd24425e0df0efc5bce3ef58
Li Ling <liling@kth.se>
2023-05-22 13:38:30 +0200

    Main: add minimum code to load test data

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 from argparse import ArgumentParser
 from lib.utils import load_config, setup_seed
 from easydict import EasyDict as edict
+from datasets import mbes_data
+from torch.utils.data import DataLoader
 setup_seed(0)
+
+def get_datasets(config: edict):
+    if(config.dataset=='multibeam'):
+        train_set, val_set = mbes_data.get_multibeam_train_datasets(config)
+        test_set = mbes_data.get_multibeam_test_datasets(config)
+    else:
+        raise NotImplementedError
+
+    return train_set, val_set, test_set
+
+def test(config: edict):
+    _, _, test_set = get_datasets(config)
+    test_loader = DataLoader(test_set, batch_size=config.batch_size, num_workers=config.num_workers, shuffle=False)
+    for i, data in enumerate(test_loader):
+        print(data['matching_inds'].shape)
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -10,3 +28,4 @@ if __name__ == '__main__':
     config = load_config(args.config)
     config = edict(config)
     print(config)
+    test(config)
