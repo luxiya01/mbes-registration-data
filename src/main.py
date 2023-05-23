9f7f08cef175eeaa4d408af8e0c996e427ed4eb9
Li Ling <liling@kth.se>
2023-05-23 18:41:24 +0200

    Main: replace o3d registration metrics with feature match recalls

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from argparse import ArgumentParser
 from easydict import EasyDict as edict
 from torch.utils.data import DataLoader
+import torch
 import numpy as np
 import open3d as o3d
 import copy
 import pandas as pd
+from tqdm import tqdm
 
 from lib.utils import load_config, setup_seed
 from lib.benchmark_utils import to_o3d_pcd, to_tsfm
+from lib.evaluations import fmr_wrt_distances, fmr_wrt_inlier_ratio, get_predicted_correspondence_gt_distance
 from datasets import mbes_data
 setup_seed(0)
 
@@ -28,20 +31,18 @@ def draw_registration_results(source, target, transform_gt, transform_pred):
 def generalized_icp(config: edict,
                     data: dict,
                     estimation_method=o3d.pipelines.registration.TransformationEstimationForGeneralizedICP()):
-    points_src = to_o3d_pcd(data['points_src'].squeeze(0))
-    points_ref = to_o3d_pcd(data['points_ref'].squeeze(0))
-    transform_gt = to_tsfm(data['transform_gt_rot'].squeeze(0), data['transform_gt_trans'].squeeze(0))
+    points_src = to_o3d_pcd(data['points_src'])
+    points_ref = to_o3d_pcd(data['points_ref'])
+    transform_gt = to_tsfm(data['transform_gt_rot'], data['transform_gt_trans'])
 
     predicted = o3d.pipelines.registration.registration_generalized_icp(
         points_src, points_ref, config.overlap_radius, np.eye(4),
         estimation_method,
         criteria=o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=200))
 
-    eval_gt = o3d.pipelines.registration.evaluate_registration(points_src, points_ref, config.overlap_radius, transform_gt)
-    eval_pred = o3d.pipelines.registration.evaluate_registration(points_src, points_ref, config.overlap_radius, predicted.transformation)
-    print(f'Ground truth: {eval_gt}\nPredicted: {eval_pred}')
-    draw_registration_results(points_src, points_ref, transform_gt, predicted.transformation)
-    return {'gt': eval_gt, 'pred': eval_pred}
+    if config.draw_registration_results:
+        draw_registration_results(points_src, points_ref, transform_gt, predicted.transformation)
+    return predicted.transformation
 
 
 def get_datasets(config: edict):
@@ -57,21 +58,28 @@ def test(config: edict):
     _, _, test_set = get_datasets(config)
     test_loader = DataLoader(test_set, batch_size=config.batch_size, num_workers=config.num_workers, shuffle=False)
 
-    gt_fitness, gt_inlier_rmse, gt_corr = [], [], []
-    pred_fitness, pred_inlier_rmse, pred_corr = [], [], []
+    gt_all_distances = []
+    pred_all_distances = []
 
-    for i, data in enumerate(test_loader):
-        res = generalized_icp(config, data)
-        gt_fitness.append(res['gt'].fitness)
-        gt_inlier_rmse.append(res['gt'].inlier_rmse)
-        gt_corr.append(np.array(res['gt'].correspondence_set).shape[0])
-        pred_fitness.append(res['pred'].fitness)
-        pred_inlier_rmse.append(res['pred'].inlier_rmse)
-        pred_corr.append(np.array(res['pred'].correspondence_set).shape[0])
-        #print(data['matching_inds'].shape)
-    df = pd.DataFrame.from_dict({'gt_fitness': gt_fitness, 'gt_inlier_rmse': gt_inlier_rmse, 'gt_corr': gt_corr,
-                    'pred_fitness': pred_fitness, 'pred_inlier_rmse': pred_inlier_rmse, 'pred_corr': pred_corr})
-    df.to_csv(f'{config.noise_type}_{config.subset_test}_gicp_res.csv', index=False)
+    for _, data in tqdm(enumerate(test_loader), total=len(test_set)):
+        for key in data.keys():
+            if isinstance(data[key], torch.Tensor):
+                data[key] = data[key].squeeze(0)
+        transform_pred = generalized_icp(config, data)
+        pred_distances = get_predicted_correspondence_gt_distance(data, transform_pred)
+        pred_all_distances.append(pred_distances)
+
+        transform_gt = to_tsfm(data['transform_gt_rot'], data['transform_gt_trans'])
+        gt_distances = get_predicted_correspondence_gt_distance(data, transform_gt)
+        gt_all_distances.append(gt_distances)
+
+    print('Using ground truth GT:')
+    fmr_wrt_distances(gt_all_distances)
+    fmr_wrt_inlier_ratio(gt_all_distances)
+    print('==========================================')
+    print('Using predicted transformation:')
+    fmr_wrt_distances(pred_all_distances)
+    fmr_wrt_inlier_ratio(pred_all_distances)
 
 
 if __name__ == '__main__':
